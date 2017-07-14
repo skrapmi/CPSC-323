152c0db648cdd30768c4541189987078acf2c304
Stephen Cordasco <stephencordasco@gmail.com>
2017-07-14 06:42:08 -0700

    Updated the main.cpp file to take any command line argument and store that argument as a filename. Added another INI test file (test2.ini)

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -9,15 +9,25 @@
 using namespace std;
 
 // Begin main function
-int main() {
-  // Create an instance of IniFile
-  IniFile stuff("test.ini");
+int main(int argc, char *argv[0]) {
+  // Used to store any filename passed as a command argument
+  std::string aFile;
+
+  // Check for a command argument
+  if (argc != 2) {
+    cout << "usage: " << argv[0] << " <filename>\n";
+  } else {
+    // store the command argument as the filename
+    aFile = argv[1];
+  }
+  // Create an instance of IniFile and pass the filename from the command line
+  IniFile stuff(aFile);
 
   // Extract elements from the INI file
-	cout << stuff.GetProfileString("foo", "bar") << endl;
+  cout << stuff.GetProfileString("foo", "bar") << endl;
   cout << stuff.GetProfileString("foo2", "shift") << endl;
-	cout << stuff.GetProfileString("second", "shift") << endl;
-	cout << stuff.GetProfileString("second", "bar") << endl;
+  cout << stuff.GetProfileString("second", "shift") << endl;
+  cout << stuff.GetProfileString("second", "bar") << endl;
 
   return 0;
 }
