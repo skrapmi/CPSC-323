edeaf6afb590083ef218bf98e4029723261542cf
Stephen Cordasco <stephencordasco@gmail.com>
2017-07-14 12:05:47 -0700

    Fixed spacing with inifile.cpp

--- Assignment1/inifile.cpp
+++ Assignment1/inifile.cpp
@@ -71,34 +71,33 @@ IniFile::IniFile(std::string filename)
 
   // Begin do-while loop
   do {
-    getline(inFile, buf);
-  
-     // Begin try-catch block
-     try {
-	  // Checks for RE matching Section and ignors the rest
+    getline(inFile, buf);  
+    // Begin try-catch block
+    try {
+      // Checks for RE matching Section and ignors the rest
       if (regex_search(buf, match, sec) && !regex_search(buf, comMatch, com)) {
-		secStr = match.str(1);
-		data.clear();
-	  // Checks for RE matching Property/Value combo and ignors the rest
+	secStr = match.str(1);
+       	data.clear();
+	// Checks for RE matching Property/Value combo and ignors the rest
       }	else if(regex_search(buf, match, prop) && !regex_search(buf, comMatch, com)) {
-      	//check for empty map before navigation
-		if(!map_data.empty()){
-			map_data_it = map_data.begin();
-			// While If block to reset data for additional properties in same section
-			while(map_data_it != map_data.end()){
-				if(map_data_it->first == secStr){
-					data.clear();
-					data = map_data_it->second;
-					map_data.erase(map_data_it);
-					break;
-				}
-				map_data_it++;
-			}
-		}		
-		propStr = match.str(1);
-		valStr = match.str(2);
+	//check for empty map before navigation
+       	if(!map_data.empty()){
+	  map_data_it = map_data.begin();
+	  // While If block to reset data for additional properties in same section
+	  while(map_data_it != map_data.end()){
+	    if(map_data_it->first == secStr){
+	      data.clear();
+	      data = map_data_it->second;
+	      map_data.erase(map_data_it);
+	      break;
+	    }
+	    map_data_it++;
+	  }
+	}		
+       	propStr = match.str(1);
+	valStr = match.str(2);
 		
-		// All past and current properties/values mapped and written to data
+       	// All past and current properties/values mapped and written to data
         data[propStr] = valStr; 
         // data now linked to a single section
         map_data[secStr] = data;
@@ -116,18 +115,18 @@ IniFile::IniFile(std::string filename)
 
   cout << "\n ======MAP=========\n";
   while(map_data_it != map_data.end())
+    {
+      cout << "\n  SECTION: " << map_data_it->first << endl;
+      data = map_data_it->second;
+      data_it = data.begin();
+      while(data_it != data.end())
 	{
-	  cout << "\n  SECTION: " << map_data_it->first << endl;
-	  data = map_data_it->second;
-	  data_it = data.begin();
-	  while(data_it != data.end())
-	    {
-	     cout << " Property: " << data_it->first << endl;
-		  cout << "    Value: " << data_it->second << endl;
-		  data_it++;
-		}
-	  map_data_it++;
-	}
+	  cout << " Property: " << data_it->first << endl;
+       	  cout << "    Value: " << data_it->second << endl;
+	  data_it++;
+       	}
+      map_data_it++;
+    }
   
   // Close the file
   inFile.close();
@@ -141,34 +140,31 @@ IniFile::IniFile(std::string filename)
  ***************************************************************************/
 string IniFile::GetProfileString(std::string section, std::string property_name)
 {
-	// Iterators for the maps
-	std::map< std::string, std::map<std::string, std::string>>::iterator map_data_it;
-	std::map<std::string, std::string>::iterator data_it;
-	// Initialize the tracking of section found or not
-	bool secFound = false;
-	// Set the map_data iterator to the beginning of the map
-  	map_data_it = map_data.begin();
-	// While If While If - to find and return the requested value
-	while(map_data_it != map_data.end())
-	{
-		if(map_data_it->first == section)
-		{
-			secFound = true;
-			data = map_data_it->second;
-			data_it = data.begin();
-			while(data_it != data.end())
-			{
-				if(data_it->first == property_name)
-					return data_it->second;
-				data_it++;
-			}
-		}
-		map_data_it++;
-	}
-	//Output for not found section or properties
-	if(secFound)
-		return "Not a valid property of section: " + section;
-	else
-		return "Section " + section + " not found";
+  // Iterators for the maps
+  std::map< std::string, std::map<std::string, std::string>>::iterator map_data_it;
+  std::map<std::string, std::string>::iterator data_it;
+  // Initialize the tracking of section found or not
+  bool secFound = false;
+  // Set the map_data iterator to the beginning of the map
+  map_data_it = map_data.begin();
+  // While If While If - to find and return the requested value
+  while(map_data_it != map_data.end()) {
+    if(map_data_it->first == section) {
+      secFound = true;
+      data = map_data_it->second;
+      data_it = data.begin();
+      while(data_it != data.end()) {
+	if(data_it->first == property_name)
+	  return data_it->second;
+	data_it++;
+      }
+    }
+    map_data_it++;
+  }
+  //Output for not found section or properties
+  if(secFound)
+    return "Not a valid property of section: " + section;
+  else
+    return "Section " + section + " not found";
 }
 
