ff45563ab7f09516222be0375ec338964e21e081
glezluis <luismg@csu.fullerton.edu>
2017-07-30 22:55:18 -0700

    most is done just need to generate labels and regs

--- Assignment3/luis/genCode.ml
+++ Assignment3/luis/genCode.ml
@@ -37,35 +37,45 @@ let rec expEval = function
   | Call (cf, cargs) -> 
       let funcEval = function
           Func (fid, fdta, fdtb) -> match fid with
-          |"getint" -> Printf.printf "\na1 := &input\ncall %s\n" fid
-          |"putint" -> Printf.printf "\na1 := &output\ncall %s\n" fid
+          |"getint" -> Printf.printf "a1 := &input\ncall readint\n" 
+          |"putint" -> Printf.printf "a1 := &output\n";
+              Printf.printf "r1 := ";
+              let argsEval = function
+                |[] -> Printf.printf "expected int argument...\n"
+                |h::_ -> expEval h ;
+              in argsEval cargs; 
+                Printf.printf "\na2 = r1\n"; 
+                Printf.printf "call writeint\n";
           | _ -> Printf.printf "%s is not a valid function call...\n" fid
-
       in funcEval cf ;
-        (*Printf.printf "with args ";
-          let rec argsEval = function
-          |[] -> ()
-          |h::t -> expEval h ; argsEval t 
-          in argsEval cargs *)
-  | Neq (ne, ne2) -> Printf.printf "r1: = "; expEval ne ;
+  | Neq (ne, ne2) -> Printf.printf "\nr1: = "; expEval ne ;
       Printf.printf "\nr2 := "; expEval ne2; 
-      Printf.printf "\nr1 := r1 != r2\n if r1 goto L3\n";
-  |Gt (ge, ge2) -> expEval ge; Printf.printf " > "; expEval ge2; 
-  |Minus (me, me2) -> expEval me; Printf.printf "minus "; expEval me2; 
+      Printf.printf "\nr1 := r1 != r2\n";
+  |Gt (ge, ge2) -> Printf.printf "r1 := "; expEval ge; 
+      Printf.printf"\nr2 := "; expEval ge2; Printf.printf "\nr1 = r1 > r2\n";
+      expEval ge2; Printf.printf " := r1\n"
+  |Minus (me, me2) -> Printf.printf "r1 := "; expEval me; 
+      Printf.printf"\nr2 := "; expEval me2; Printf.printf "\nr1 = r1 - r2\n";
 ;;
 
 
 let startP = function
-    Program stmtList -> 
+    Program stmtList -> Printf.printf "main:\n";
       let rec trav = function
         | [] -> ()
         | Assign(aVar, aExp)::t -> expEval aExp;
             (varEval aVar); Printf.printf" := r1 \n"  ; trav t
         | Expr eExp::t ->  expEval eExp; trav t
-        | While (wExp, wList)::t -> Printf.printf "while "; expEval wExp;
-            Printf.printf "\n  do: "; trav wList; trav t
-        | If (ifExp, thenList, elseList)::t -> Printf.printf "L2: "; 
-            expEval ifExp; trav t
+        | While (wExp, wList)::t -> Printf.printf "goto L1\n";
+            Printf.printf "L2:\n";
+            trav wList;  Printf.printf "L1: "; expEval wExp; 
+            Printf.printf "if r1 goto L2\n";
+            trav t
+        | If (ifExp, thenList, elseList)::t ->  
+            expEval ifExp; Printf.printf "if r1 goto L3\n";
+            trav elseList; Printf.printf "goto L4\nL3: \n";
+            trav thenList; Printf.printf "L4:\n";
+            trav t
       in trav stmtList;
         Printf.printf "a1 := &output\ncall writeln\ngoto exit\n"
 ;;
